1f47a97a895533af369127f0fa9065f2b2577924
Chris van Wagoner <chris@buzzshift.com>
2016-06-05 09:31:14 -0500

    removed unused, commented-out code

--- peacock/peacock.theme.bash
+++ peacock/peacock.theme.bash
@@ -4,11 +4,8 @@ SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX=" |"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
-# GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_DIRTY=" "
-# GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 GIT_THEME_PROMPT_CLEAN=" ✨ 🙌 ✨ "
-# GIT_THEME_PROMPT_PREFIX=" ${green}|"
 GIT_THEME_PROMPT_PREFIX="${reset_color}  🌱  "
 GIT_THEME_PROMPT_SUFFIX=" "
 
@@ -21,17 +18,11 @@ SCM_GIT_UNTRACKED_CHAR="❔  ${bold_white}"
 SCM_GIT_UNSTAGED_CHAR="${bold_yellow} 🤔  "
 SCM_GIT_STAGED_CHAR="${bold_yellow} 🙏  "
 
-
-
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
 function prompt_command() {
-    # PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    #
-    # PS1="\n$(battery_char) $(clock_char) ${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
-    #
-    PS1="\n${bold_cyan}$(date +"📅  %m/%d 🕔  %H:%M")$(scm_prompt_info)\n${bold_green}\w\n${reset_color}>🔮  "
+    PS1="\n${bold_cyan}$(date +"📅  %m/%d 🕔  %H:%M")$(scm_prompt_info)\n${bold_green}\w\n${reset_color}🔮  "
 }
 
 PROMPT_COMMAND=prompt_command;
