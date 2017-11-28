c08658def293b91e4e9f67bc5e73fe8df5090c7a
Chris van Wagoner <chrisvanwagoner@users.noreply.github.com>
2017-11-28 13:35:45 -0500

    Update peacock.theme.bash
    Removed a line break to make it 2 lines tall, removed some emoji, and added text descriptions

--- peacock/peacock.theme.bash
+++ peacock/peacock.theme.bash
@@ -5,24 +5,24 @@ SCM_THEME_PROMPT_PREFIX=" |"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 GIT_THEME_PROMPT_DIRTY=" "
-GIT_THEME_PROMPT_CLEAN=" ✨ 🙌 ✨ "
-GIT_THEME_PROMPT_PREFIX="${reset_color}  🌱  "
+GIT_THEME_PROMPT_CLEAN=" ${reset_color}✨clean✨ "
+GIT_THEME_PROMPT_PREFIX=" ${reset_color}🌱branch:"
 GIT_THEME_PROMPT_SUFFIX=" "
 
 SCM_GIT='git'
 SCM_GIT_CHAR='±'
-SCM_GIT_DETACHED_CHAR='😱  '
-SCM_GIT_AHEAD_CHAR=" 🚀  ${bold_green}"
-SCM_GIT_BEHIND_CHAR=" ☄️  ${bold_blue}"
-SCM_GIT_UNTRACKED_CHAR="❔  ${bold_white}"
-SCM_GIT_UNSTAGED_CHAR="${bold_yellow} 🤔  "
-SCM_GIT_STAGED_CHAR="${bold_yellow} 🙏  "
+SCM_GIT_DETACHED_CHAR=" ${reset_color}😱detached:"
+SCM_GIT_AHEAD_CHAR=" 🚀${bold_cyan}push:"
+SCM_GIT_BEHIND_CHAR=" ☄️ ${bold_blue}pull:"
+SCM_GIT_UNTRACKED_CHAR=" ${bold_purple}untracked:"
+SCM_GIT_UNSTAGED_CHAR=" ${bold_red}unstaged:"
+SCM_GIT_STAGED_CHAR=" ${bold_yellow}staged:"
 
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
 function prompt_command() {
-    PS1="\n$(battery_char) ${bold_cyan}$(date +"📅  %m/%d 🕔  %H:%M")$(scm_prompt_info)\n${bold_green}\w\n${reset_color}🔮  "
+    PS1="\n${bold_cyan}$(date +"%m/%d %H:%M") ${bold_green}\w$(scm_prompt_info)\n${reset_color}🔮 "
 }
 
 PROMPT_COMMAND=prompt_command;
