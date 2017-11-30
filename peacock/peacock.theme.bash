042e1848d9740f3cc1e0c2b66f89c494f837cc7e
Chris van Wagoner <chrisvanwagoner@gmail.com>
2017-11-30 08:15:02 -0500

    More emoji removed

--- peacock/peacock.theme.bash
+++ peacock/peacock.theme.bash
@@ -4,16 +4,16 @@ SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX=" |"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
-GIT_THEME_PROMPT_DIRTY=" "
-GIT_THEME_PROMPT_CLEAN=" ${reset_color}✨clean✨ "
-GIT_THEME_PROMPT_PREFIX=" ${reset_color}🌱branch:"
+GIT_THEME_PROMPT_DIRTY=" ${bold_red}dirty "
+GIT_THEME_PROMPT_CLEAN=" ${{bold_green}clean "
+GIT_THEME_PROMPT_PREFIX=" ${reset_color}branch:"
 GIT_THEME_PROMPT_SUFFIX=" "
 
 SCM_GIT='git'
 SCM_GIT_CHAR='±'
-SCM_GIT_DETACHED_CHAR=" ${reset_color}😱detached:"
-SCM_GIT_AHEAD_CHAR=" 🚀${bold_cyan}push:"
-SCM_GIT_BEHIND_CHAR=" ☄️ ${bold_blue}pull:"
+SCM_GIT_DETACHED_CHAR=" ${reset_color}detached:"
+SCM_GIT_AHEAD_CHAR=" ${bold_cyan}push:"
+SCM_GIT_BEHIND_CHAR=" ${bold_blue}pull:"
 SCM_GIT_UNTRACKED_CHAR=" ${bold_purple}untracked:"
 SCM_GIT_UNSTAGED_CHAR=" ${bold_red}unstaged:"
 SCM_GIT_STAGED_CHAR=" ${bold_yellow}staged:"
